ebb070c55228eba1f82bda8698225f75d78c667d
Ruqaya <Ruqaya.phcs@gmail.com>
2023-07-09 11:41:50 +0300

    profile

--- routes/profile.js
+++ routes/profile.js
@@ -7,6 +7,8 @@ const profileController = require('../controllers/profile');
 
 router.get('/profile/view' , isLoggedIn , profileController.view_profile_get)
 router.get('/courses/index', isLoggedIn, profileController.course_index_get)
+router.post('/course/delete', profileController.course_delete)
+
 
 module.exports = router;
 
