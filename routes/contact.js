192071212a016dcb29ed6a17dd3eb35b1df29105
Sumaya Alyas <elias2sumaya@gmail.com>
2023-07-09 08:07:16 +0300

    contact files2

--- routes/contact.js
+++ routes/contact.js
@@ -5,5 +5,5 @@ const contactControllers = require('../controllers/contact')
 //Routes here
 router.get('/contact/index', contactControllers.contact_index_get)
 router.post('/contact/add', contactControllers.contact_add_post)
-
+//exports
 module.exports = router
